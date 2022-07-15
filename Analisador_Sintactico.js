06857c1d978ae5f31704b23f007e59ed2ac79d80
AlboresMagin <201271@ids.upchiapas.edu.mx>
2022-07-15 09:03:46 -0500

    ultima version del programa

--- Analisador_Sintactico.js
+++ Analisador_Sintactico.js
@@ -1,6 +1,8 @@
 let validation = false;
 var cadena = "";
 var lineas = [];
+var info = [];
+let pila = [];
 
 var tabla_predictiva = [["", "CUERPOACUOSO", "temperatura", "grasas", "huevosHelmintos", "fecales", "PH", "UT", "ARROLLO", "RIO", "CANAL", "200-250", "340-400", "460-500", "SST", "DO", "CO", "F", "N", "NA", "AP", "2-5", "6-9", "24-35", "100-180", ":", ";", "(", ")", "<", ">", "{", "}"],
 ["S", ["CA", "PI", "PRCA", "PF", "IC", "CCL", "FC"], "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
@@ -20,13 +22,13 @@ var tabla_predictiva = [["", "CUERPOACUOSO", "temperatura", "grasas", "huevosHel
 ["RNGRAS", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ["100-180", "LIM"], "", "", "", "", "", "", "", ""],
 ["HELMINTO", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ["PI", "MI", "FHELMINTO", "MF", "PF", "PP", "PHEL"], "", "", "", "", ""],
 ["FHELMINTO", "", "", "", "huevosHelmintos", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
-["PHEL", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ["NA", "LIM"], "", "", "", "", "", "", "", "", "", "", "", "", ""],
+["PHEL", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ["NA", "LIM"], ["AP", "LIM"], "", "", "", "", "", "", "", "", "", "", "", ""],
 ["FECAL", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ["PI", "MI", "FFECAL", "MF", "PF", "PP", "FESG"], "", "", "", "", ""],
 ["FFECAL", "", "", "", "", "fecales", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
 ["FESG", "", "", "", "", "", "", "", ["ARROLLO", "RFCL2"], ["RIO", "RFCL1"], ["CANAL", "RFCL3"], "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
 ["RFCL1", "", "", "", "", "", "", "", "", "", "", ["200-250", "LIM"], "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
 ["RFCL2", "", "", "", "", "", "", "", "", "", "", "", ["340-400", "LIM"], "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
-["RFCL1", "", "", "", "", "", "", "", "", "", "", "", "", ["460-500", "LIM"], "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
+["RFCL3", "", "", "", "", "", "", "", "", "", "", "", "", ["460-500", "LIM"], "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""],
 ["PH", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ["PI", "MI", "PH", "MF", "PF", "PP", "RNPH"], "", "", "", "", ""],
 ["RNPH", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ["6-9", "LIM"], "", "", "", "", "", "", "", "", "", ""],
 ["UT", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ["PI", "MI", "UT", "MF", "PF", "PP", "RNUT"], "", "", "", "", ""],
@@ -57,24 +59,115 @@ function obtener_datos() {
     console.log(cadena)
 }
 
+function obtener_info(elementos) {
+    info.push(elementos[2])
+    info.push(parseInt(elementos[13]))
+    info.push(parseInt(elementos[22]))
+    info.push(elementos[30])
+    info.push(elementos[38])
+    info.push(parseInt(elementos[39]))
+    info.push(parseInt(elementos[47]))
+    info.push(parseInt(elementos[55]))
+}
+
+function validar_canal() {
+    if (info[1] >= 24 && info[1] <= 26) {
+        if (info[2] >= 100 && info[2] <= 120) {
+            if (info[3] == "NA") {
+                if (info[4] == "CANAL") {
+                    if (info[5] >= 460 && info[5] <= 500) {
+                        if (info[6] >= 7 && info[6] <= 8) {
+                            if (info[7] == 3) {
+                                window.location.href = 'consumoHumano.html'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function validar_NextPage(){
+    if (info[1] >= 27 && info[1] <= 32) {
+        if (info[2] >= 130 && info[2] <= 155) {
+            if (info[3] == "NA") {
+                if (info[4] == "ARROLLO") {
+                    if (info[5] >= 340 && info[5] <= 400) {
+                        if (info[6] == 6) {
+                            if (info[7] == 4) {
+                                window.location.href = 'actividadesDomesticas.html'
+                            }
+                        }
+                    }
+                }else if(info[4] == "RIO"){
+                    if (info[5] >= 200 && info[5] <= 250) {
+                        if (info[6] == 6) {
+                            if (info[7] == 4) {
+                                window.location.href = 'actividadesDomesticas.html'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }else if(info[1] >= 33 && info[1] <= 35){
+        if(info[2] >= 160 && info[2] <= 180){
+            if (info[3] == "AP") {
+                if (info[4] == "ARROLLO") {
+                    if (info[5] >= 340 && info[5] <= 400) {
+                        if (info[6] == 9) {
+                            if (info[7] == 5) {
+                                window.location.href = 'tratamiento.html'
+                            }
+                        }
+                    }
+                }else if(info[4] == "RIO"){
+                    if (info[5] >= 200 && info[5] <= 250) {
+                        if (info[6] == 9) {
+                            if (info[7] == 5) {
+                                window.location.href = 'tratamiento.html'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 function validar() {
     obtener_datos();
     separar_cadena_lineas();
+    let lineas_aux = lineas.slice();
     algoritmo_tabla_predictiva()
+    if (pila.size() == 0 && lineas.length == 0) {
+        alert("Cadena valida")
+        obtener_info(lineas_aux);
+        console.log(info);
+        if (info[0] == "CANAL") {
+            validar_canal();
+        }else if(info[0] == "ARROLLO"||"RIO"){
+            validar_NextPage();
+        }
+    }
+    else {
+        alert("Cadena invalida")
+    }
 }
 
 function algoritmo_tabla_predictiva() {
-    let pila = new Stack();
+    pila = new Stack();
     pila.push("S");
     while (lineas.length != 0) {
         console.log("Entrada :", lineas[0])
         console.log("Pila: ", pila.peek())
-        if (lineas[0] == pila.peek() || pila.peek() == "RNTEM" || pila.peek() == "RNGRAS" || pila.peek() == "RFCL2" || pila.peek() == "RNPH" || pila.peek() == "RNUT") {
+        if (lineas[0] == pila.peek() || pila.peek() == "RNTEM" || pila.peek() == "RNGRAS" || pila.peek() == "RFCL2" || pila.peek() == "RNPH" || pila.peek() == "RNUT" || pila.peek() == "RFCL1" || pila.peek() == "RFCL3") {
             let aux2 = parseInt(lineas[0])
             switch (pila.peek()) {
                 case "RNTEM":
-                    if (aux2 > 24 && aux2 <= 35) {
+                    if (aux2 >= 24 && aux2 <= 35) {
                         pila.pop();
                         pila.push("LIM")
                         lineas.shift()
@@ -84,7 +177,7 @@ function algoritmo_tabla_predictiva() {
                     }
                     break;
                 case "RNGRAS":
-                    if (aux2 > 100 && aux2 <= 180) {
+                    if (aux2 >= 100 && aux2 <= 180) {
                         pila.pop();
                         pila.push("LIM")
                         lineas.shift()
@@ -94,7 +187,27 @@ function algoritmo_tabla_predictiva() {
                     }
                     break;
                 case "RFCL2":
-                    if (aux2 > 340 && aux2 <= 400) {
+                    if (aux2 >= 340 && aux2 <= 400) {
+                        pila.pop();
+                        pila.push("LIM")
+                        lineas.shift()
+                    }
+                    else {
+                        alert("Error en la entrada")
+                    }
+                    break;
+                case "RFCL1":
+                    if (aux2 >= 200 && aux2 <= 250) {
+                        pila.pop();
+                        pila.push("LIM")
+                        lineas.shift()
+                    }
+                    else {
+                        alert("Error en la entrada")
+                    }
+                    break;
+                case "RFCL3":
+                    if (aux2 >= 460 && aux2 <= 500) {
                         pila.pop();
                         pila.push("LIM")
                         lineas.shift()
@@ -104,7 +217,7 @@ function algoritmo_tabla_predictiva() {
                     }
                     break;
                 case "RNPH":
-                    if (aux2 > 6 && aux2 <= 9) {
+                    if (aux2 >= 6 && aux2 <= 9) {
                         pila.pop();
                         pila.push("LIM")
                         lineas.shift()
@@ -114,7 +227,7 @@ function algoritmo_tabla_predictiva() {
                     }
                     break;
                 case "RNUT":
-                    if (aux2 > 2 && aux2 <= 5) {
+                    if (aux2 >= 2 && aux2 <= 5) {
                         pila.pop();
                         pila.push("LIM")
                         lineas.shift()
@@ -124,7 +237,7 @@ function algoritmo_tabla_predictiva() {
                     }
                     break;
                 case "RNUT":
-                    if (aux2 > 200 && aux2 <= 250) {
+                    if (aux2 >= 200 && aux2 <= 250) {
                         pila.pop();
                         pila.push("LIM")
                         lineas.shift()
@@ -134,7 +247,7 @@ function algoritmo_tabla_predictiva() {
                     }
                     break;
                 case "RNUT":
-                    if (aux2 > 460 && aux2 <= 500) {
+                    if (aux2 >= 460 && aux2 <= 500) {
                         pila.pop();
                         pila.push("LIM")
                         lineas.shift()
@@ -185,12 +298,6 @@ function algoritmo_tabla_predictiva() {
                 }
             }
         }
-    }
-    if(pila.size() == 0 && lineas.length == 0){
-        alert("Cadena valida")
-    }
-    else{
-        alert("Cadena invalida")
     }
 }
 
